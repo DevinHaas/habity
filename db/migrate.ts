b45d9b5911742cc14656eeb55cd2980d947d8adf
Devin Halser <devin.hasler@bluewin.ch>
2026-01-12 17:47:37 +0100

    fix: change drizzle path for Dockerfile

--- db/migrate.ts
+++ db/migrate.ts
@@ -22,7 +22,7 @@ async function runMigration() {
 
   const db = drizzle(client);
   try {
-    await migrate(db, { migrationsFolder: "./.drizzle/migrations" });
+    await migrate(db, { migrationsFolder: "./db/migrations" });
     console.log("Migration completed ✅");
   } catch (error) {
     console.error("Migration failed 🚨:", error);
